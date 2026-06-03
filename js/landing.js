1fab282cce4482618fd11681e4dcbe5c53539db1
juhyuk061210 <juhyukkim0612@gmail.com>
2026-06-03 22:16:20 +0900

    Add margin calculator benefit

--- js/landing.js
+++ js/landing.js
@@ -51,6 +51,7 @@
     if (bundleFeatures) {
       bundleFeatures.innerHTML = '<li><span class="plan__check">✓</span><span>책 1권 (약 238페이지)</span></li>' +
         '<li><span class="plan__check">✓</span><span class="plan__feat-strong">일 매출 500찍는 제품 리스트</span></li>' +
+        '<li><span class="plan__check">✓</span><span class="plan__feat-strong">제품 마진 계산기</span></li>' +
         '<li><span class="plan__check">✓</span><span class="plan__feat-strong">일 매출 100단위 멘토들에게 컨설팅</span></li>' +
         '<li><span class="plan__check">✓</span><span class="plan__feat-strong">직접 실행하는 사람들과 커뮤니티</span></li>' +
         '<li><span class="plan__check">✓</span><span>7일 환불 보장</span></li>';
